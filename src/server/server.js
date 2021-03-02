5f1cfa5316dd955fc3dc3e195e13cfb7f0267156
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-03-02 16:11:19 -0600

    Usando Nodemon y Dotenv

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,20 @@
-import express from 'express' //importamos express
+import express from 'express'; //importamos express
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const { ENV, PORT } = process.env;
 const app = express();
+
+if (ENV === 'development') {
+  console.log(ENV);
+}
 
 app.get('*', (req, res) => {
   res.send({ hello: 'express' });
 });
 
-app.listen(3000, (err) => {
+app.listen(PORT, (err) => {
   if (err) console.error();
   else console.log('Server running on port: 3000');
 });
