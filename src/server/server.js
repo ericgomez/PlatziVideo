8a84dd58f70eb3e7b904791f388c87dd2a679545
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-03-02 17:23:51 -0600

    Definición de la función principal para realizar el renderizado desde el servidor

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,15 @@
 import express from 'express'; //importamos express
 import dotenv from 'dotenv';
 import webpack from 'webpack';
+import React from 'react';
+import { renderToString } from 'react-dom/server'; //Funcion para renderear los componentes como string
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { renderRoutes } from 'react-router-config'; //router
+import { StaticRouter } from 'react-router-dom';
+import serverRoutes from '../frontend/routes/serverRoutes';
+import reducer from '../frontend/reducers';
+import initialState from '../frontend/initialState';
 
 dotenv.config();
 
@@ -27,21 +36,34 @@ if (ENV === 'development') {
 
 }
 
-app.get('*', (req, res) => {
-  res.send(`
-  <!DOCTYPE html>
-    <html>
-      <head>
-        <link rel="stylesheet" href="assets/app.css" type="text/css"/> 
-        <title>Platzi Video</title>
-      </head>
-      <body>
-        <div id="app"></div>
-        <script src="assets/app.js" type="text/javascript"></script>
-      </body>
-    </html>
-  `);
-});
+const setResponse = (html) => {
+  return(`<!DOCTYPE html>
+  <html>
+    <head>
+      <link rel="stylesheet" href="assets/app.css" type="text/css"/> 
+      <title>Platzi Video</title>
+    </head>
+    <body>
+      <div id="app">${html}</div>
+    </body>
+    <script src="assets/app.js" type="text/javascript"></script>
+  </html>`);
+}
+
+const renderApp = (req, res) => {
+  const store = createStore(reducer, initialState);
+  const html = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={{}}>
+        {renderRoutes(serverRoutes)}
+      </StaticRouter>
+    </Provider>
+  ); //con esta funcion preparamos el provider para el redux y el router, 
+  //dentro del router colocamos la funcion renderRoutes y le pasamos el archivo de las rutas
+  res.send(setResponse(html));
+}
+
+app.get('*', renderApp);
 
 app.listen(PORT, (err) => {
   if (err) console.error();
